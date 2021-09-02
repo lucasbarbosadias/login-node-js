c71515e04aa27a7880a70d57f2eb4aa8cd01b0a1
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-09-02 13:45:37 -0300

    feature: adding initial files and project dependencies

---
+++ index.js
@@ -0,0 +1,17 @@
+import register from './routes/register.js'
+import login from './routes/login.js'
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+const { SERVER } = process.env
+
+app.use(cors())
+app.use(express.json())
+
+app.use(register)
+app.use(login)
+
+app.listen(SERVER)
